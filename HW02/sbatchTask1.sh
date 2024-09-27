f9b3585f72c513f0cf0f04e30003dd8a307938f0
Sai Kartik <f20200435@pilani.bits-pilani.ac.in>
2024-09-27 16:06:14 -0500

    update sbatch for task1

--- HW02/sbatchTask1.sh
+++ HW02/sbatchTask1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 #SBATCH --partition=instruction             ##Use the instruction partition
-#SBATCH --time=0-00:01:00
+#SBATCH --time=0-00:10:00
 #SBATCH -c 2
 #SBATCH --job-name=HW02_task1
 #SBATCH -o task1.out -e task1.err ## Set the output file and error file
@@ -9,6 +9,9 @@
 cd $SLURM_SUBMIT_DIR
 
 g++ scan.cpp task1.cpp -Wall -O3 -std=c++17 -o task1
-./task1 1000
+
+for ((i = 1; i <= 30; i++)); do
+    ./task1 $((2**i));
+done
 
 
