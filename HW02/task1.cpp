9d15aaff11c8c27bff54588a5cbb2ee87a6238ad
Sai Kartik <f20200435@pilani.bits-pilani.ac.in>
2024-10-04 11:42:23 -0500

    modify task1.cpp

--- HW02/task1.cpp
+++ HW02/task1.cpp
@@ -10,17 +10,20 @@ using namespace std;
 using chrono::duration;
 using chrono::high_resolution_clock;
 
+const int MIN_VAL = -1;
+const int MAX_VAL = 1;
+
 int main(int argc, char *argv[]){
 
     size_t n = stoi(argv[1]); //take in command line input for length of array
 
     random_device entropy_source;
     mt19937_64 generator(entropy_source());
-    uniform_real_distribution<float> dist(-1.0, 1.0);
+    uniform_real_distribution<float> dist(MIN_VAL, MAX_VAL);
     
     float *scanned_arr = (float *)malloc(sizeof(float) * n); //allocate memory for the scanned array
 
-    //declare timer functions
+    //declare timing points
     high_resolution_clock::time_point start;
     high_resolution_clock::time_point end;
     duration<double, milli> duration_millisec;
@@ -30,6 +33,7 @@ int main(int argc, char *argv[]){
         value = dist(generator); //populate the array with random values
     }
 
+    //start timing
     start = high_resolution_clock::now(); //start clock
     scan(arr, scanned_arr, n);
     end = high_resolution_clock::now(); //end timing
