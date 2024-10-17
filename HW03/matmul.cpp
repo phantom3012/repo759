3dde615d73c52487bef2d83d57c42f2406d993e8
Sai Kartik <f20200435@pilani.bits-pilani.ac.in>
2024-10-17 14:40:38 -0500

    modify task1

--- HW03/matmul.cpp
+++ HW03/matmul.cpp
@@ -1,7 +1,7 @@
 #include "matmul.h"
 
 void mmul(const float* A, const float* B, float* C, const std::size_t n){
-    #pragma omp parallel for collapse(3)
+    #pragma omp parallel for collapse(2) schedule(static)
     for(size_t i = 0; i < n; i++){
         for(size_t k = 0; k < n; k++){
             for(size_t j = 0; j < n; j++){
