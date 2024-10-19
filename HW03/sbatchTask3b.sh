a709418f2c0ffb281f63ef1ff589d33c8f025a85
Sai Kartik <f20200435@pilani.bits-pilani.ac.in>
2024-10-18 19:58:27 -0500

    modify sbatch task3b

--- HW03/sbatchTask3b.sh
+++ HW03/sbatchTask3b.sh
@@ -12,5 +12,5 @@ cd $SLURM_SUBMIT_DIR
 g++ task3.cpp msort.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
 
 for ((i = 1; i <= 20; i++)); do
-    ./task3 1000000 $((i)) 8; #replace 8 with best value for ts found in 3a
+    ./task3 1000000 $((i)) 2**10;
 done
