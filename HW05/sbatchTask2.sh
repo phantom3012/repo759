d3ebdfaf4e352e35296ea61700c186d0f18895e9
Sai Kartik <f20200435@pilani.bits-pilani.ac.in>
2024-11-07 16:42:00 -0600

    modify cuda versions used

--- HW05/sbatchTask2.sh
+++ HW05/sbatchTask2.sh
@@ -10,7 +10,7 @@
 
 cd $SLURM_SUBMIT_DIR
 
-module load nvidia/cuda/11.8.0
+module load nvidia/cuda/12.0.0
 module load gcc/11.3.0
 
 nvcc task2.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task2
