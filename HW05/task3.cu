0511aae14a11240c16134c8c58aaf668ee5b62c5
Sai Tadinada <vtadinada@wisc.edu>
2024-11-07 10:48:31 -0600

    modify task3

--- HW05/task3.cu
+++ HW05/task3.cu
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <random>
 
+#include "vscale.cu"
+
 const int THREADS = 512;
 
-int main() {
+int main(int argc, char *argv[]) {
     //create generators for random numbers
     std::random_device entropy_source;
     std::mt19937 generator(entropy_source());
-    std::uniform_real_distribution<int> distA(-10, 10);
-    std::uniform_real_distribution<int> distB(-1, 1);
+    std::uniform_real_distribution<float> distA(-10, 10);
+    std::uniform_real_distribution<float> distB(-1, 1);
 
     //CUDA timing events
     cudaEvent_t start, stop;
@@ -19,8 +21,8 @@ int main() {
     int n = std::stoi(argv[1]); //get the number of elments from the command line
 
     //generate the random arrays a and b
-    float a[n];
-    float b[n];
+    const float *a = (float*) malloc(n*sizeof(float)) ;
+    float *b = (float*) malloc(n*sizeof(float));
 
     //fill the arrays with random numbers corresponding to their range
     for(int i = 0; i < n; i++) {
@@ -29,8 +31,8 @@ int main() {
     }
 
     for(int i = 0; i < n; i++) {
-        cout << "a[" << i << "] = " << a[i] << "\n";
-        cout << "b[" << i << "] = " << b[i] << "\n";
+	    std::cout << "a[" << i << "] = " << a[i] << "\n";
+	    std::cout << "b[" << i << "] = " << b[i] << "\n";
     }
 
     int numberOfBlocks = (n + THREADS -1)/THREADS;
@@ -42,9 +44,9 @@ int main() {
 
     cudaEventElapsedTime(&elapsedTime, start, stop);
 
-    cout << elapsedTime << "\n";
-    cout << b[0] << "\n";
-    cout << b[n-1] << "\n";
+    std:: cout << elapsedTime << "\n";
+    std::cout << b[0] << "\n";
+    std::cout << b[n-1] << "\n";
 
     //clean up
     cudaEventDestroy(start);
