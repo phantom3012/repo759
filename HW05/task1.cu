718b8c766272168c9cb3e0c8b1758754a6b90ad5
Sai Kartik <f20200435@pilani.bits-pilani.ac.in>
2024-11-06 18:49:08 -0600

    fix indentation mismatch

--- HW05/task1.cu
+++ HW05/task1.cu
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 __global__ void factorialKernel(){
-   int a = threadIdx.x+1; //Get the number of the factorial we want to find (given as a in the question)
-   int b = 1; // declare a variable to hold the factorial value (given as b in the question)
-   for (int i = 1 ; i <=a ; i++){
-	  b *= i;
-   }
+    int a = threadIdx.x+1; //Get the number of the factorial we want to find (given as a in the question)
+    int b = 1; // declare a variable to hold the factorial value (given as b in the question)
+    for (int i = 1 ; i <=a ; i++){
+        b *= i;
+    }
    printf("%d!=%d\n", a , b); 
     
 }
