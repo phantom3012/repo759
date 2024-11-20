87fe84582e21c3f599d1c830b6f5e3eb93157cd6
Sai Kartik <f20200435@pilani.bits-pilani.ac.in>
2024-11-20 02:13:53 -0600

    modify sbatch

--- HW06/sbatchTask1.sh
+++ HW06/sbatchTask1.sh
@@ -13,7 +13,7 @@ cd $SLURM_SUBMIT_DIR
 module load nvidia/cuda/12.0.0
 module load gcc/11.3.0
 
-nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task1
+nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o task1
 
 for ((i = 5; i <= 14; i++)); do
     ./task1 $((2**i)) 1024; # threads_per_block = 1024
