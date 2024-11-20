fa34d29ffd0f5f5f7b3ec6e044dea10a8b5e16d7
Sai Kartik <f20200435@pilani.bits-pilani.ac.in>
2024-11-20 16:02:13 -0600

    modify matmul

--- HW06/matmul.cu
+++ HW06/matmul.cu
@@ -2,34 +2,38 @@
 #include <cmath>
 #include <cuda.h>
 
-// Computes the matrix product of A and B, storing the result in C.
-// Each thread should compute _one_ element of output.
-// Does not use shared memory for this problem.
-//
-// A, B, and C are row major representations of nxn matrices in device memory.
-//
-// Assumptions:
-// - 1D kernel configuration
 __global__ void matmul_kernel(const float* A, const float* B, float* C, size_t n){
 
-    int tx = threadIdx.x + blockIdx.x * blockDim.x;
-    int ty = threadIdx.y + blockIdx.y * blockDim.y;
-
-    if ( tx < n && ty < n){
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    if (index < n*n){
+        int row_num = index / n;
+        int col_num = index % n;
         float sum = 0;
-        for (std::size_t k = 0; k < n; k++){
-            sum += A[tx * n + k] * B[k * n + ty];
+        for (int i = 0; i < n; i++){
+            sum += A[row_num * n + i] * B[i * n + col_num];
         }
-        C[tx * n + ty] = sum;
+        C[row_num * n + col_num] = sum;
     }
+
+    // int tx = threadIdx.x + blockIdx.x * blockDim.x;
+    // int ty = threadIdx.y + blockIdx.y * blockDim.y;
+
+    // if ( tx < n && ty < n){
+    //     float sum = 0;
+    //     for (std::size_t k = 0; k < n; k++){
+    //         sum += A[tx * n + k] * B[k * n + ty];
+    //     }
+    //     C[tx * n + ty] = sum;
+    // }
 }
 
-// Makes one call to matmul_kernel with threads_per_block threads per block.
-// You can consider following the kernel call with cudaDeviceSynchronize (but if you use 
-// cudaEventSynchronize to time it, that call serves the same purpose as cudaDeviceSynchronize).
 void matmul(const float* A, const float* B, float* C, size_t n, unsigned int threads_per_block){
-    dim3 griddim((n+sqrt(threads_per_block)-1 / sqrt(threads_per_block)), (n+sqrt(threads_per_block)-1 / sqrt(threads_per_block)));
-    dim3 blockdim(sqrt(threads_per_block), sqrt(threads_per_block));
-    matmul_kernel<<<griddim, blockdim>>>(A, B, C, n);
+    // dim3 griddim((n+sqrt(threads_per_block)-1 / sqrt(threads_per_block)), (n+sqrt(threads_per_block)-1 / sqrt(threads_per_block)));
+    // dim3 blockdim(sqrt(threads_per_block), sqrt(threads_per_block));
+
+    int numBlocks = (n+threads_per_block-1) / threads_per_block;
+    dim3 threadsPerBlock(threads_per_block);
+    dim3 numBlocks(numBlocks);
+    matmul_kernel<<<numBlocks, threadsPerBlock>>>(A, B, C, n);
     cudaDeviceSynchronize();
 }
