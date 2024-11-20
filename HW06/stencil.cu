4f78702eaddca193c950d11fb11c2962b0c5890e
Sai Kartik <f20200435@pilani.bits-pilani.ac.in>
2024-11-20 17:04:03 -0600

    implement task2

---
+++ HW06/stencil.cu
@@ -0,0 +1,47 @@
+#include <cuda.h>
+
+#include "stencil.cuh"
+
+__global__ void stencil_kernel(const float* image, const float* mask, float* output, unsigned int n, unsigned int R){
+    extern __shared__ float shared_mem[];
+
+    float *shared_image = shared_mem;
+    float *shared_mask = shared_mem + blockDim.x + 2 * R;
+
+    std::size_t threadIndex = threadIdx.x;
+    std::size_t globalIndex = blockIdx.x * blockDim.x + threadIndex;
+
+    if(threadIndex < 2 * R + 1){
+        shared_mask[threadIndex] = mask[threadIndex];
+    }
+
+    int shared_start = blockIdx.x * blockDim.x - R;
+    int shared_end = shared_start + blockDim.x + 2 * R;
+
+    if(shared_start + threadIndex >= 0 && shared_start + threadIndex < n){
+        shared_image[threadIndex] = image[shared_start + threadIndex];
+    } else {
+        shared_image[threadIndex] = 1;
+    }
+
+    __syncthreads();
+
+    if(globalIndex < n){
+        float result = 0;
+        for(int i = 0; i < 2 * R + 1; i++){
+            result += shared_image[threadIndex + i] * shared_mask[i];
+        }
+        output[globalIndex] = result;
+    }
+
+}
+
+__host__ void stencil(const float* image, const float* mask, float* output, unsigned int n, unsigned int R, unsigned int threads_per_block){
+
+    int numOfBlocks = (n+threads_per_block-1) / threads_per_block;
+    int sharedMemorySize = sizeof(float) * (threads_per_block + 2 * R) + sizeof(float) *(2 * R + 1);
+
+    stencil_kernel<<<numOfBlocks, threads_per_block, sharedMemorySize>>>(image, mask, output, n, R);
+    cudaDeviceSynchronize();
+
+}
