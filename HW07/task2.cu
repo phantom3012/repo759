bcb8e01f9a1353b40356f3bacf3c63ae3d2b9f0f
Sai Kartik <f20200435@pilani.bits-pilani.ac.in>
2024-12-06 02:37:26 -0600

    remove debug statemen

--- HW07/task2.cu
+++ HW07/task2.cu
@@ -24,9 +24,6 @@ int main(int argc, char* argv[]){
     float *input = (float*) malloc(n * sizeof(float));
     for(std::size_t i = 0; i < n; i++){
         input[i] = dist_float(generator);
-        if(i < 10){
-            std::cout << input[i] << std::endl;
-        }
     }
     float *d_input, *d_output;
 
